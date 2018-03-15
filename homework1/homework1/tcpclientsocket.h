b73c35b5f5fbcb8c64b8783833ac2bf3da2dd4b9
YunLambert <469066072@qq.com>
2018-03-16 01:06:43 +0800

    update

--- homework1/homework1/tcpclientsocket.h
+++ homework1/homework1/tcpclientsocket.h
@@ -1,4 +1,20 @@
 #ifndef TCPCLIENTSOCKET_H
 #define TCPCLIENTSOCKET_H
 
+#include <QDialog>
+#include <QObject>
+
+class TcpClientSocket:public QDialog
+{
+    Q_OBJECT
+public:
+    TcpClientSocket(QObject *parent=0);
+
+signals:
+    void updateClients(QString,int);
+    void disconnected(int);
+
+protected slots:
+    void slotDisconnected();
+};
 #endif // TCPCLIENTSOCKET_H
