2417b5aaa7cb2b909f0d53bf9885b829326c7038
YunLambert <469066072@qq.com>
2018-05-02 13:08:35 +0800

    因为要与服务器端对接，改变了showdir的返回格式

--- homework3/showdir/dir.c
+++ homework3/showdir/dir.c
@@ -11,13 +11,21 @@ int main(void)
 {
 	DIR *dir;    
 	struct dirent *file;    
-	int found = 0;       
+	int found = 0;    
+	char buf[MAX_LEN] = "";
 	dir = opendir("E:\\Workstation\\Python");   //这里改成需要显示文件目录的文件位置    
 	if (dir) 
 	{ 
-		while ((file = readdir(dir)) != NULL) 
-		    printf("%s\n", file->d_name); 
+		while ((file = readdir(dir)) != NULL)
+		{
+			//printf("%s\n", file->d_name); 
+			strcat_s(buf, sizeof(buf), file->d_name);   //文件全部存到buf中
+			strcat_s(buf, sizeof(buf), "  ");  //这里可以改变输出格式，文件是几个空格相隔还是换行符相隔
+		}
 	} 
+	printf("%s ", buf); 
+
+	//return buf;       //返回的是一个包含了所有文件的字符串类型
 	system("pause");
 	return 0;
 }
